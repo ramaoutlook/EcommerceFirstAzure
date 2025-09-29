e7985ec634277292042cb43e8481f167177b43c4
ramaoutlook <47940749+ramaoutlook@users.noreply.github.com>
2025-09-29 11:40:25 +0530

    Front Page listing all products in the Home page.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,27 @@
 using System.Diagnostics;
+using Ecommerce8.Data;
 using Ecommerce8.Models;
+using Ecommerce8.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce8.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private ApplicationDbContext _context;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var products = await _context.Products.ToListAsync();
+            var model = new HomeViewModel {  Products = products };
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Ecommerce8.Models;
+
+namespace Ecommerce8.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Product> Products { get; set; }
+    }
+}
